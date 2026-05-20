db8cd531eecf59ee5fe99ad8b372a27b2c0132a9
bakhtkhoshl50179-source <bakhtkhoshl50179@masseyhigh.school.nz>
2026-05-21 11:52:42 +1200

    Factors

---
+++ B_01_factors.py
@@ -0,0 +1,125 @@
+# Generates headings (eg:----- Heading -----)
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# Display instructions
+def instructions():
+    statement_generator("Instructions", "-")
+
+    print("""
+Instructions go here.
+To use this program simply enter an integer between 
+1 and 200. The program will show the factors of your
+chosen integer.
+
+It will also tell you if your chosen number...
+- is a prime number (ie: it has two factors)
+- is a perfect square
+
+To exit the program, please type 'xxx'. """)
+
+
+# Ask user for an integer between 1 and 200.
+def num_check(question):
+    error = "Please enter a number that is between 1 and 200 inclusive.\n"
+    while True:
+
+        response = input(question).lower()
+        if response == "xxx":
+            return response
+
+        try:
+            # ask the user for a number
+            response = int(response)
+
+            # check that the number is between 1 and 200
+            if 1 <= response <= 200:
+                return response
+            else:
+                print(error)
+
+        except ValueError:
+            print(error)
+
+
+# Works out factors, returns sorted list
+def factor(var_to_factor):
+    factors_list = []
+
+    # square root the number to work out when to stop working
+    stop = var_to_factor ** 0.5
+    stop = int(stop)
+
+    for item in range (1, stop + 1):
+
+        # check to see if the item is a factor
+        if to_factor % item == 0:
+            factors_list.append(item)
+
+
+            # Calculate partner
+            partner = to_factor // item
+
+            # Add partner to the list (but prevent duplicate entries)
+            if partner not in factors_list:
+               factors_list.append(partner)
+
+
+    # return the sorted list
+    factors_list.sort()
+    return factors_list
+
+# Main Routine Goes Here
+
+statement_generator("The Ultimate Factor Finder","-")
+
+# Display instructions if requested
+want_instructions = input("\nPress <enter> to read the instructions "
+                          "or any key to continue ")
+if want_instructions == "":
+    instructions()
+
+while True:
+
+    comment = ""
+
+    # ask user for number to be factorised
+    to_factor = num_check("\nEnter an integer (or xxx to quit): ")
+    print("You chose to factor", to_factor)
+
+    if to_factor == "xxx":
+         break
+
+    # get factors for integers that are 2 or more
+    elif to_factor != 1:
+        all_factors = factor(to_factor)
+
+    # set up comment for unity
+    else:
+         all_factors = ""
+         comment = "One is UNITY! It only has one factor. Itself :)"
+
+     # comments for squares / primes
+
+    # Prime numbers only have two factors
+    if len(all_factors) == 2:
+        comment = f"{to_factor} is a prime number"
+
+    # check if the list has an odd number of factors
+    elif len(all_factors) % 2 == 1:
+        comment = f"{to_factor} is a perfect square"
+
+    # Set up headings
+    if to_factor == 1:
+        heading = "One is special..."
+    else:
+        heading = f"Factors of {to_factor}"
+
+    # output factors and comment
+    print()
+    statement_generator(heading, "*")
+    print(all_factors)
+    print(comment)
+
+    print("Thank you for using the factors calculator")
